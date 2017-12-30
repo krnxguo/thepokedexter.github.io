0010c9eba644ce487448076c6e8e4393dd96b593
Karen Guo <karenguo@Karens-MacBook-Air.local>
2017-12-29 21:08:54 -0800

    Use vue init project that includes bootstrap

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -22,7 +22,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -36,14 +36,17 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }

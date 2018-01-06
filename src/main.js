25e0d057b20cb4142e236f390f9d0645a869d776
Karen Guo <karenguo@live.com>
2018-01-05 22:29:58 -0800

    Use vuetify webpack-advanced quickstart

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
 import App from './App'
 import router from './router'
-import Vuetify from 'vuetify'
-import('../node_modules/vuetify/dist/vuetify.min.css')
 
-Vue.config.productionTip = false
 Vue.use(Vuetify)
 
-import ConctactInfo from './components/ContactInfo.vue'
-Vue.component('contact-info', ConctactInfo)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App, ConctactInfo }
+  components: { App }
 })

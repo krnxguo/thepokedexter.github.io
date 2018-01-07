003e88c38d133b9fcc22e1dee0a8f213f7bca1ab
Karen Guo <karenguo@live.com>
2018-01-06 23:41:51 -0800

    Create introduction component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <v-app>
     <v-container grid-list-xl>
       <contact-info></contact-info>
+      <introduction></introduction>
     </v-container>
   </v-app>
 </template>
 
 <script>
 import ContactInfo from './components/ContactInfo'
+import Introduction from './components/Introduction'
 
 export default {
-  components: {ContactInfo},
+  components: {
+    ContactInfo,
+    Introduction
+  },
   data () {
     return {
     }
---
+++ src/components/Introduction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="introduction">
+    Intro
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>

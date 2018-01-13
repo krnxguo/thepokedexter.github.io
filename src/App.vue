2919d93756c2cc78b9b59853d4b34e0614eb3327
Karen Guo <karenguo@live.com>
2018-01-12 21:26:08 -0800

    Remove unused variables

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import ContactInfo from './components/ContactInfo'
 import Introduction from './components/Introduction'
 import Skills from './components/Skills'
 
-import data from './assets/data/skills'
+import skills from './assets/data/skills'
 
 export default {
   components: {
@@ -28,22 +28,7 @@ export default {
   },
   data () {
     return {
-      skills: data,
-      programmingLanguages: [
-        {label: 'Javascript', rating: 5},
-        {label: 'Typescript', rating: 5},
-        {label: 'HTML/CSS', rating: 5},
-        {label: 'C#', rating: 3},
-        {label: 'C++', rating: 3},
-        {label: 'Python', rating: 2}
-      ],
-      frameworks: [
-        {label: 'React', rating: 5},
-        {label: 'Angular', rating: 5},
-        {label: 'Node.js', rating: 4},
-        {label: '.NET', rating: 3},
-        {label: 'Vue.js', rating: 3}
-      ]
+      skills: skills
     }
   }
 }

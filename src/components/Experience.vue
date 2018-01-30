6dcfc14d4cdf5dd18c9b008ff780993cad10f930
Karen Guo <karenguo@live.com>
2018-01-29 23:55:19 -0800

    Separate work experience cards

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,15 +3,15 @@
     <v-layout>
       <v-flex xs12 sm10 lg8 offset-xs0 offset-sm1 offset-lg2>
         <h3 id="title" class="display-2">{{ title }}</h3>
-        <job
-          v-for="(job) in experience" :key="job.employer + job.startDate"
-          :employer="job.employer"
-          :startDate="job.startDate"
-          :endDate="job.endDate"
-          :position="job.position"
-        ></job>
       </v-flex>
     </v-layout>
+    <job
+      v-for="(job) in experience" :key="job.employer + job.startDate"
+      :employer="job.employer"
+      :startDate="job.startDate"
+      :endDate="job.endDate"
+      :position="job.position"
+    ></job>
   </div>
 </template>
 
@@ -35,6 +35,6 @@ export default {
   text-align: center;
   color: #494949;
   margin-top: 1.5rem;
-  margin-bottom: 1.5rem;
+  margin-bottom: 1rem;
 }
 </style>
--- src/components/common/Job.vue
+++ src/components/common/Job.vue
@@ -1,19 +1,25 @@
 <template>
-  <v-card>
-    <v-toolbar class="light-blue" dark flat>
-      <v-toolbar-title>{{ employer }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>{{ startDate + " - " + endDate}}</v-toolbar-title>
-    </v-toolbar>
-    <v-container>
-      <h5 class="headline position">{{ position }}</h5>
-      <ul>
-        <li>
-          <span class="subheading">Designed and implemented a C++ program to manipulate MRIs for analysis</span>            
-        </li>
-      </ul>
-    </v-container>
-  </v-card>
+  <div id="job">
+    <v-layout>
+      <v-flex xs12 sm10 lg8 offset-xs0 offset-sm1 offset-lg2>
+        <v-card>
+          <v-toolbar class="light-blue" dark flat>
+            <v-toolbar-title>{{ employer }}</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>{{ startDate + " - " + endDate}}</v-toolbar-title>
+          </v-toolbar>
+          <v-container>
+            <h5 class="headline position">{{ position }}</h5>
+            <ul>
+              <li>
+                <span class="subheading">Designed and implemented a C++ program to manipulate MRIs for analysis</span>            
+              </li>
+            </ul>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>

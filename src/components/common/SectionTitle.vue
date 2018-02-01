38bc935253b592d47ebc18f080837493470e3080
Karen Guo <karenguo@live.com>
2018-01-31 22:26:32 -0800

    Refactoring titles into own component

---
+++ src/components/common/SectionTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm10 lg8 offset-xs0 offset-sm1 offset-lg2>
+      <h3 class="display-1 section-title">{{ title }}</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>

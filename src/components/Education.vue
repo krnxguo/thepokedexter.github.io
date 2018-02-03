9b371e4fc4120d7b30d9606d11bf4f9543b6c07e
Karen Guo <karenguo@live.com>
2018-02-03 14:40:11 -0800

    Add text for UBC

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -5,12 +5,19 @@
       <v-flex xs12 sm10 lg8 offset-xs0 offset-sm1 offset-lg2>
         <v-card dark color="pink">
           <v-container>
-            <v-flex xs2>
-              <span class="fa-stack fa-2x">
-                <i class="fa fa-circle fa-stack-2x"></i>
-                <i class="fa fa-graduation-cap fa-stack-1x fa-inverse"></i>
-              </span>
-            </v-flex>
+            <v-layout row wrap>
+              <v-flex xs2>
+                <span class="fa-stack fa-2x">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-graduation-cap fa-stack-1x fa-inverse"></i>
+                </span>
+              </v-flex>
+              <v-flex xs10>
+                <h5 class="headline">{{ education.school }}</h5>
+                <h6 class="title">{{ education.degree + ', ' + education.major }}</h6>
+                <span class="subheading">{{ 'Graduated in ' + education.graduationDate }}</span>
+              </v-flex>
+            </v-layout>
           </v-container>
         </v-card>
       </v-flex>
